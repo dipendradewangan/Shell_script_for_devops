3fe8bf06e96e0955d90e72935f7a8a0403b1d65e
= <=>
2025-08-25 17:12:31 +0000

    created a shell script for deploying djaongo aap

---
+++ day03/deploy_django.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+
+
+
+<< task
+Deploy a django app
+and handel the ccode for errors
+task
+
+
+
+
+
+
+code_clone(){
+	echo "Cloning the Django app......"
+	git clone https://github.com/LondheShubham153/django-notes-app.git
+}
+
+
+install_requirements(){
+	echo "Installing Dependencies"
+	sudo apt-get update
+	sudo apt-get install docker.io nginx -y
+
+}
+
+
+
+required_restarts(){
+	sudo chown $USER /var/run/docker.sock
+	sudo systemctl enable docker
+	sudo systemctl enable nginx
+	sudo systemctl restart docker
+}
+
+
+deploy(){
+	docker build -t notes-app .
+	docker run -d -p 8000:8000 notes-app:latest
+}
+
+
+echo "******************* Deployment Start ************************"
+
+
+
+
+if ! code_clone; then
+	echo "The code directory already exists.................."
+	cd django-notes-app
+fi
+
+
+
+if ! install_requirements; then
+	echo "Installation Failed..........."
+	exit 1
+fi
+
+
+if ! required_restarts; then 
+	echo "System fault identified.....!"
+	exit 1
+fi
+
+
+
+if ! deploy; then
+	echo "Deployment failed,  mailing the admin"
+fi
+
+
+echo "******************* Deployment Done *************************"
---
+++ day03/error_handling.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+create_directory(){
+
+	mkdir demo
+
+	echo "Created demo folder successfully"
+}
+
+
+
+create_directory
+
+
+echo "This code should not be execute"
+
+
+
+
